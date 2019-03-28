e5b82d00eef695a0777789824786d18015519521
cuom1999 <lephuocdinh99@gmail.com>
2019-03-27 19:58:46 -0500

    Change Main.cpp

--- ChamBai/src/Main.cpp
+++ ChamBai/src/Main.cpp
@@ -5,11 +5,8 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0);
 
-    Judge * judge = new Judge();
-
-    judge->judgeAll();
-
-    delete judge;
+	Judge judge;
+    judge.judgeAll();
     
     return 0;
 }
